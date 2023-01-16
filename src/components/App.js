36eb4e2af5516c511960234b711d9798cf080b41
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-01-16 20:59:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import '../styles/App.css';
 import List from "./List";
 
 const App = () => {
-
+ 
   const [value, setValue] = useState(0);
   const [list, setList] = useState([]);
 
 
   const onButtonClick = () => {
-   setList(Array.from({ length: `${value}` }, (v, i) => i))
+    setList([])
+    for (let i = 1; i < Number(value)+1; i++) {
+      
+      setList((prev) => [...prev, i]);
+    }
+   
   };
-
+ 
 
   const onInputChange = (e) => {
     setValue(e.target.value);
@@ -23,15 +28,8 @@ const App = () => {
 
       <input id="input" onChange={onInputChange} />
       <button id="button" onClick={onButtonClick}>Click</button>
-      <ul id="list">
-
-        {
-
-          list.map((item,i) => {
-            return <List listx={i+1}/>
-          })
-        }
-
+       <ul id="list">
+      <List listx={list} />
       </ul>
     </div>
   );
