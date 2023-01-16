e41db4dc9781ca901ddb4db84466382ee16aef3e
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-01-16 20:59:51 +0530

    Update ListItems.js

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 const ListItems = (props) => {
-  return;
-  <React.Fragment>
-    <li className="items">{props.valuex}</li>;
-  </React.Fragment>;
+  return <li className="items">Hello the value is {props.valuex}</li>;
 };
 export default ListItems;
